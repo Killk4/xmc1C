1f5eaac26dc7815a5fb7da24c858399792466ec1
Borisevich V <vasily.borisevich@gmail.com>
2025-04-22 09:29:17 +0300

    FIX log

--- ui/main_ui.py
+++ ui/main_ui.py
@@ -135,9 +135,11 @@ class Ui_MainWindow(object):
             if int(text[:-1]) == 100:
                 self.end_time = time.time()
                 elapsed_time = self.end_time - self.start_time
-                self.processView.setHtml(f'{self.log_text}Скрипт завершил работу за {str(elapsed_time)} секунд.\n')
-            return None
-        self.processView.setHtml(self.log_text)
+                self.processView.append(f'Скрипт завершил работу за {str(elapsed_time)} секунд.\n')
+        else:
+            self.processView.setHtml(self.log_text)
+
+        self.processView.moveCursor(QtGui.QTextCursor.End)
 
     def log_progress(self, percent_text: str) -> None:
         self.logs(f"Обработка: {percent_text}")
